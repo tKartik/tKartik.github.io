8f0a4a39b6487e1bf53b1b912185f564e81a48cc
Kartik <tyagik2097@gmail.com>
2020-10-31 16:44:45 +0530

    Updates

---
+++ precache-manifest.e93428a95766394ddc0dfac46a0e1532.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56f3569eb97b03c3a4039dbd37c51257",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c1681c09c7558f924d21",
+    "url": "/static/css/main.12dde431.chunk.css"
+  },
+  {
+    "revision": "ced9746deae42ce452ca",
+    "url": "/static/js/2.2fa13959.chunk.js"
+  },
+  {
+    "revision": "c1681c09c7558f924d21",
+    "url": "/static/js/main.a7678e55.chunk.js"
+  },
+  {
+    "revision": "32e85939abfc5d08bae5",
+    "url": "/static/js/runtime-main.b2451916.js"
+  },
+  {
+    "revision": "9cc22c0dd3095662ca31fa7acd59ba8c",
+    "url": "/static/media/logo3.9cc22c0d.png"
+  },
+  {
+    "revision": "0f5a20f154d1b8dbca4043a8f8d528ab",
+    "url": "/static/media/peep.0f5a20f1.png"
+  },
+  {
+    "revision": "2a456740f31d867b7554b4ac562b536f",
+    "url": "/static/media/peep2.2a456740.png"
+  },
+  {
+    "revision": "32779635391ff61fb0c6003784faa44e",
+    "url": "/static/media/rec.32779635.jpg"
+  }
+]);
